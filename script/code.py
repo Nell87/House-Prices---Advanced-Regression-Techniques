7f91edf4b900510e838a1489f7a0f867cb2522fd
Sara <sara.marlop87@gmail.com>
2021-02-15 23:44:16 +0100

    Data

---
+++ script/code.py
@@ -0,0 +1,13 @@
+
+#### 0.   INCLUDES / PREPARING DATA _______________________________________ #### 
+# Loading Libraries
+import pandas as pd  # reading csv 
+import os
+from pathlib import Path
+
+
+# Reading datasets
+train = pd.read_csv("../data/train.csv")
+
+
+
