fee18e3cbe735eee061b2f6888459080bfb10f42
Sara <sara.marlop87@gmail.com>
2021-02-16 00:59:05 +0100

    First steps

--- script/code.py
+++ script/code.py
@@ -1,13 +1,40 @@
 
-#### 0.   INCLUDES / PREPARING DATA _______________________________________ #### 
+#### 0.   INCLUDES / PREPARING DATA ______________________________________ #### 
 # Loading Libraries
 import pandas as pd  # reading csv 
-import os
-from pathlib import Path
-
 
 # Reading datasets
-train = pd.read_csv("../data/train.csv")
+train = pd.read_csv("C:/Sara/GitHub/House Prices - Advanced Regression Techniques/data/train.csv")
+test = pd.read_csv("C:/Sara/GitHub/House Prices - Advanced Regression Techniques/data/test.csv")
 
+# Summary
+summary = train.describe()
+train.shape         # (1460,81)
+test.shape          # (1459,80)
 
+del summary
 
+#### 1.   CLEANING / PREPROCESSING ####
+
+#### 1.1. Missing Values __________________________________________________ ####
+train_missing = train.isnull().sum() * 100 / len(train)
+test_missing = test.isnull().sum() * 100 / len(test)
+
+thresh = 20*train.shape[0]/100
+train = train.dropna(axis='columns', thresh=(thresh)) # Let's remove more than 80% missing
+                                                      # 81 --> 77 variables
+                                                      
+variables_train = list(train.columns)
+variables_train.remove('SalePrice')                                         
+                                                      
+test = test[variables_train]
+
+del thresh
+del train_missing
+del test_missing
+del variables_train
+
+#### 2.   EXPLORATORY ANALYSIS ####
+train.len()
+
+#### 2.1. DV: SalePrice __________________________________________________ ####
